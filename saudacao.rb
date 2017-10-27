52be1d4c4db28470c63d52cfc37384b394b95a94
matheus <matheusbtmendes@gmail.com>
2017-10-27 14:28:16 -0300

    iniciando script

---
+++ saudacao.rb
@@ -0,0 +1,16 @@
+# A classe Saudação
+class Saudacao
+  def initialize(name)
+    @name = name.capitalize
+  end
+
+  def sauda
+    puts "Olá #{@name}!"
+  end
+end
+
+# Criar um novo objecto
+ola = Saudacao.new("mundo")
+
+# Saída: "Olá Mundo!"
+ola.sauda

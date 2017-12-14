51ea37b805e034c7e6576ff19fcd1158721b135f
matheusb982 <mbtm_133@hotmail.com>
2017-12-14 16:24:59 -0300

    Update input_two_window.rb

--- input_two_window.rb
+++ input_two_window.rb
@@ -1,4 +1,6 @@
 require 'gtk3'
+require "pp"
+require "serialport"
 include Gtk
 
 Gtk.init
@@ -16,13 +18,13 @@ end
 box = VBox.new 10
 
 text = Entry.new
-text.set_text "Digite o baud rate"
+text.set_text ""
 box.pack_start text
 
 button = Gtk::Button.new(:label => "Inserir")
-
 button.signal_connect "clicked" do
     puts "Hello "+text.text+"!"
+    @baud_rate = text.text
     new_window(text.text)
 end
 
@@ -37,7 +39,7 @@ def new_window(baud_rate)
   window2.set_window_position(:center)
 
   window2.show
-  window2.set_title "Controlador Serial Port"
+  #window2.set_title "Controlador Serial Port"
   window2.signal_connect "destroy" do
       Gtk.main_quit
   end
@@ -47,13 +49,62 @@ def new_window(baud_rate)
   button = Gtk::Button.new(:label => "Inserir")
 
   button.signal_connect "clicked" do
-      puts baud_rate
+      #puts baud_rate
+      check_connection
   end
 
   box.pack_start button
 
   window2.add box
   window2.show_all
+end
+
+def check_connection
+  retur = true
+  ports=Dir.glob("/dev/ttyUSB*")
+  if ports.size != 1
+    printf("Cabo não conectado /dev/ttyUSB* serial\n")
+    retur = false
+    #on_erro
+    #exit(1)
+  end
+
+  if retur == true
+    read_write_sp(ports)
+  end
+end
+
+def read_write_sp(ports)
+  port = ports[0]
+  #baud_rate = 4800
+  data_bits = 8
+  stop_bits = 1
+  parity = SerialPort::NONE
+
+  sp = SerialPort.new(port, @baud_rate.to_i, data_bits, stop_bits, parity)
+
+  d=0x16
+
+  sleep(0.01)
+
+  sp.flush()
+
+  sp.putc(d)
+
+  sp.flush()
+
+  printf("# W : 0x%02x\n", d.ord)
+
+  sp.flush()
+
+  c=sp.read(1)
+
+  printf("# R : 0x%02x\n", c.ord)
+
+  sp.flush()
+
+  sleep(0.01)
+  #dialog
 end
 
 Gtk.main
